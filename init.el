07b0a1bf6b86e9d0681789d2d32dd3d66bf293b1
Coque Couto <coque.couto@gmail.com>
2023-06-01 19:53:25 +0200

    Move my functions to a new section

--- init.el
+++ init.el
@@ -70,17 +70,6 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; This function joins all the lines of a paragraph in one.
-(defun jcv/unfill-paragraph (&optional region)
-  "Takes a multi-line paragraph and makes it into a single line of text."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max))
-    ;; This would override `fill-column' if it's an integer.
-    (emacs-lisp-docstring-fill-column t))
-    (fill-paragraph nil region)))
-(global-set-key
-  (kbd "C-c c q") 'jcv/unfill-paragraph)
-
 ;; Show line numbers at the left.
 (global-display-line-numbers-mode)
 
@@ -101,6 +90,20 @@
 (global-set-key
   (kbd "C-c w")
   (lambda () (interactive) (kill-new (file-name-nondirectory buffer-file-name))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; My own functions.
+
+;; This function joins all the lines of a paragraph in one.
+(defun jcv/unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+    ;; This would override `fill-column' if it's an integer.
+    (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+(global-set-key
+  (kbd "C-c c q") 'jcv/unfill-paragraph)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Additional packages
