18d4248f36a6e09f84a6d7caae835d04eb292070
Coque Couto <coque.couto@gmail.com>
2023-09-24 18:42:06 +0200

    Second attempt to configure ispell dictionary in Windows

--- init.el
+++ init.el
@@ -11,7 +11,31 @@
 (add-hook 'text-mode-hook 'flyspell-mode)
 (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 (when (equal system-type 'windows-nt)
-  (setq ispell-dictionary "en_GB"))
+  (setq ispell-dictionary "english-GB")
+    (add-to-list
+     'ispell-local-dictionary-alist '(("english-GB"
+                                       "[[:alpha:]]"
+                                       "[^[:alpha:]]"
+                                       "[']"
+                                       t
+                                       ("-d" "en_GB")
+                                       nil
+                                       utf-8)
+                                      ("english-US"
+                                       "[[:alpha:]]"
+                                       "[^[:alpha:]]"
+                                       "[']"
+                                       t
+                                       ("-d" "en_US")
+                                       nil
+                                       utf-8)
+                                      ("spanish"
+                                       "[[:alpha:]]"
+                                       "[^[:alpha:]]"
+                                       "\\(?:\\`a\\`\\)"
+                                       t
+                                       ("-d" "es")
+                                       nil utf-8))))
 
 ;; Use a temporary file to save configuration changes done via menu.
 ;; This is a way to disable the configuration menu.  All the config
