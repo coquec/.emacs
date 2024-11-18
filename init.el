7b27bb7992a54e02c4e9377c5ef391f823c7b119
Coque Couto <coque.couto@gmail.com>
2024-11-18 20:58:52 +0100

    Fix indent of some code

--- init.el
+++ init.el
@@ -171,8 +171,8 @@
   "Takes a multi-line paragraph and makes it into a single line of text."
   (interactive (progn (barf-if-buffer-read-only) '(t)))
   (let ((fill-column (point-max))
-    ;; This would override `fill-column' if it's an integer.
-    (emacs-lisp-docstring-fill-column t))
+        ;; This would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
     (fill-paragraph nil region)))
 (global-set-key
   (kbd "C-c c q") 'jcv/unfill-paragraph)
